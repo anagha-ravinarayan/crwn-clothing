d30ebefebc561d04cebb04045476af76d2a67954
Anagha <anagha.ravinarayan@sap.com>
2021-01-08 16:57:19 +0530

    Shop Page, Collection and Collection Item Components and styling

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component"
+
+import SHOP_DATA from "./shop.data.js";
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        const { collections } = this.state;
+        return (
+            <div>
+                <h1>SHOP PAGE</h1>
+                {collections.map(({ id, ...otherCollectionProps }) => {
+                    return (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default ShopPage;
