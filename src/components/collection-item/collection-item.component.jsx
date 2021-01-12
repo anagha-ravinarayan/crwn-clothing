29f8a5e86cbbdfea6095037a8b5e65adca1b6f44
Anagha <anagha.ravinarayan@sap.com>
2021-01-12 18:20:13 +0530

    Code cleanup

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
+const CollectionItem = ({ name, imageUrl, price }) => {
     return (
         <div className="collection-item">
             <div
@@ -15,9 +15,8 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-
         </div>
-    )
+    );
 }
 
 export default CollectionItem;
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -7,7 +7,7 @@ const CustomButton = ({ children, isGoogleSignIn, ...otherButtonProps }) => {
         <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherButtonProps}>
             {children}
         </button>
-    )
+    );
 }
 
 export default CustomButton;
