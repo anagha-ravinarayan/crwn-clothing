4afcfe256b0b5dddc15476315a9e57765af170dc
Anagha <anagha.ravinarayan@sap.com>
2021-01-21 10:03:22 +0530

    Redux Persist for localStorage persistence of cart state in the browser

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";       // Allows to browser to cache the state in local or session storage
+import storage from "redux-persist/lib/storage";      // Local Storage hook
 
 import UserReducer from "./user/user.reducer";
 import CartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',                // start storing from which store
+    storage,
+    whitelist: ['cart']         // Reducers to persist in storage (user is handled by firebase)
+}
+
+const rootReducer = combineReducers({
     user: UserReducer,
     cart: CartReducer
 });
+
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";       // Allows to browser to cache the state in local or session storage
 import logger from "redux-logger";
 
 import RootReducer from "./root-reducer";
@@ -7,4 +8,6 @@ const middlewares = [logger];
 
 const store = createStore(RootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
