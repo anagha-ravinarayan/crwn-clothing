d31187fa3497d51f7e4925f36780282b20eb3cd3
Anagha <anagha.ravinarayan@sap.com>
2021-01-21 13:40:25 +0530

    Redux state and reducers for Directory and Shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
 
-import SHOP_DATA from "./shop.data.js";
-
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div>
-                {collections.map(({ id, ...otherCollectionProps }) => {
-                    return (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    );
-                })}
-            </div>
-        );
-    }
+const ShopPage = ({ collections }) => {
+    return (
+        <div>
+            {collections.map(({ id, ...otherCollectionProps }) => {
+                return (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                );
+            })}
+        </div>
+    );
 }
 
-export default ShopPage;
+const mapStateToProps = (state) => {
+    return ({
+        collections: selectShopCollections(state)
+    });
+}
+
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,16 +4,20 @@ import storage from "redux-persist/lib/storage";      // Local Storage hook
 
 import UserReducer from "./user/user.reducer";
 import CartReducer from "./cart/cart.reducer";
+import DirectoryReducer from "./directory/directory.reducer";
+import ShopReducer from "./shop/shop.reducer"
 
 const persistConfig = {
     key: 'root',                // start storing from which store
     storage,
-    whitelist: ['cart']         // Reducers to persist in storage (user is handled by firebase)
+    whitelist: ['cart'],         // Reducers to persist in storage (user is handled by firebase)
 }
 
 const rootReducer = combineReducers({
     user: UserReducer,
-    cart: CartReducer
+    cart: CartReducer,
+    directory: DirectoryReducer,
+    shop: ShopReducer,
 });
 
 
