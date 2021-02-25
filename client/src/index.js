135fb15d068019ca5d8d698729bd1df0c3cd9db2
Anagha <anagha.ravinarayan@sap.com>
2021-02-25 13:39:33 +0530

    PWA service-worker bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,8 @@ import { PersistGate } from "redux-persist/integration/react";
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from "./service-worker";
 
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -23,12 +22,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
