25c338d346286bfa8c8edb888edca8401940ecf9
Anagha <anagha.ravinarayan@sap.com>
2021-02-25 13:34:44 +0530

    Progressive Web App, manifest, service-worker, HTTPS

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import { PersistGate } from "redux-persist/integration/react";
 
 import './index.css';
 import App from './App';
+import * as serviceWorker from "./service-worker";
+
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
@@ -20,6 +22,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
